9b6e919a8e5e019b8fd0b4d01b262333bbac2956
Satyam Raj <satyamrajgaya434@gmail.com>
2025-04-15 21:57:11 +0530

    02: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">I am a Button</Button>
+        </div>
+        <div>
+          <Input placeholder="I am an Input" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="I am an TextArea" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
